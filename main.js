cf07f3f2ef811aada9b4fc1152166ba26341ee97
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2024-05-30 15:20:25 +0500

    add

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Q7 Number Eight: Write addition, subtraction, multiplication, and division operations that
+each result in the number 8. Enclose your operations in print statements.*/
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(8 / 1);
+/* Q8 Favorite Number: Store your favorite number in a variable. Then, create a message
+revealing your favorite number. Print that message.*/
+let favoriteNumber1 = 10;
+console.log(`My fav number is ${favoriteNumber1}`);
+export {};
