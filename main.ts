1a7f095d72ca26aa09f1409adc10f5910b3b4720
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2024-05-31 03:01:41 +0500

    add

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+// *************** 100 coding challenge "3 day" *************** //
+
+
 /* Q7 Number Eight: Write addition, subtraction, multiplication, and division operations that 
 each result in the number 8. Enclose your operations in print statements.*/
 
